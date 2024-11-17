af8d71b6e24a5f44d07b47f7a9c1ae29b654b389
joel.barba <joel.barba.vidal@gmail.com>
2024-11-17 16:57:25 +0100

    update pid scripts

--- killAll.sh
+++ killAll.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+echo ""
 pid=`ps -ax | grep -v "grep" | grep worker.js | cut -d " " -f1`
-echo "worker.js PID: $pid"
 if [ $pid ]; then
+  echo "worker.js PID: $pid"
   kill -9 $pid
   echo "Worker Killed"
+else
+  echo "worker.js PID: NOT FOUND"
 fi
 
 echo ""
-pid=`ps -ax | grep -v "grep" | grep main.js | cut -d " " -f1``
-echo "main.js PID: $pid"
+pid=`ps -ax | grep -v "grep" | grep main.js | cut -d " " -f1`
 if [ $pid ]; then
+  echo "main.js PID: $pid"
   kill -9 $pid
   echo "Main Killed"
+else
+  echo "main.js PID: NOT FOUND"
 fi
+
+echo ""
--- showPIDs.sh
+++ showPIDs.sh
@@ -1,6 +1,19 @@
-echo "worker.js PID:"
-ps -ax | grep -v "grep" | grep worker.js # | awk ${print $1}
+#!/bin/bash
 
 echo ""
-echo "main.js PID:"
-ps -ax | grep -v "grep" | grep main.js
+pid=`ps -ax | grep -v "grep" | grep worker.js | cut -d " " -f1`
+if [ $pid ]; then
+  echo "worker.js PID: $pid"
+else
+  echo "worker.js PID: NOT FOUND"
+fi
+
+echo ""
+pid=`ps -ax | grep -v "grep" | grep main.js | cut -d " " -f1`
+if [ $pid ]; then
+  echo "main.js PID: $pid"
+else
+  echo "main.js PID: NOT FOUND"
+fi
+
+echo ""
